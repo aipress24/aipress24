b1f2d3791d017a4db88dc6fbed7b00a880a95cac
Stefane Fermigier <sf@abilian.com>
2024-12-12 15:08:59 +0100

    design: tweak CSS.

--- src/app/modules/wip/constants.py
+++ src/app/modules/wip/constants.py
@@ -9,7 +9,7 @@ MENU = [
     # "cbv:app.modules.wip.crud.cbvs.articles.ArticlesWipView",
     "page:app.modules.wip.pages.newsroom.index.NewsroomPage",
     # "page:app.modules.wip.pages.comroom.index.ComRoomPage",
-    "page:app.modules.wip.pages.mail.MailPage",
+    # "page:app.modules.wip.pages.mail.MailPage",
     "page:app.modules.wip.pages.opportunities.OpportunitiesPage",
     "page:app.modules.wip.pages.business_wall.BusinessWallPage",
     "page:app.modules.wip.pages.delegate.DelegatePage",
