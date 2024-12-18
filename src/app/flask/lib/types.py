df8b0daa9e4269887d02310d8344e09b5dd39c13
Jerome Dumonteil <jd@abilian.com>
2024-12-18 10:51:04 +0100

    fix: add gain missing file flask.lib.types

---
+++ src/app/flask/lib/types.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021-2024, Abilian SAS & TCA
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+from __future__ import annotations
+
+from typing import TypeAlias
+
+JSON: TypeAlias = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
