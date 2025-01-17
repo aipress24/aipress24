17595db5dfeac4961e5ed2edda8b38f6a9b7953c
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-01-17 16:35:08 +0100

    refactor: stripe webhook log, retrieve subscription object

--- src/app/modules/stripe/views/webhook.py
+++ src/app/modules/stripe/views/webhook.py
@@ -5,11 +5,13 @@
 from __future__ import annotations
 
 import sys
+from decimal import Decimal
+from typing import Any
 
 import stripe
 from flask import request, session
 
-from app.services.stripe.utils import get_stripe_webhook_secret
+from app.services.stripe.utils import get_stripe_webhook_secret, load_stripe_api_key
 
 from .. import blueprint
 
@@ -21,6 +23,7 @@ from .. import blueprint
 
 @blueprint.route("/webhook", methods=["GET", "POST"])
 def webhooks():
+    load_stripe_api_key()
     webhook_secret = get_stripe_webhook_secret()
     if not webhook_secret:
         msg = "STRIPE_WEBHOOK_SECRET is null"
@@ -60,18 +63,167 @@ def on_checkout_session_completed(event):
     print(f"on event:{event.id}, type={event.type}", file=sys.stderr)
     data = event.data
     data_obj = data.object
+    _log_checkout_summary(data_obj)
+    # maybe in future log all checkout informations ?
+    # _store_full_checkout_information(data_obj)
+    _register_subscription(data_obj)
 
-    # for test:
-    amount_total = data_obj["amount_total"]
-    customer_details = data_obj["customer_details"]
+
+def _log_checkout_summary(data_obj: dict[str, Any]) -> None:
+    amount_total = Decimal(data_obj["amount_total"]) / 100
+    # customer_details = data_obj["customer_details"]
     customer_email = data_obj["customer_email"]
     currency = data_obj["currency"]  # "eur",
     payment_status = data_obj["payment_status"]  # "paid"
-    print(
-        f"event: {customer_email} {amount_total} {currency} {payment_status} {customer_details}",
-        file=sys.stderr,
+    client_reference_id = data_obj["client_reference_id"]  # aka org.id
+    invoice_id = data_obj["invoice"]  # in_1QiHI0IyzOgen8OqE6O66iTO
+    subscription_id = data_obj["subscription"]  # sub_1QiHI0IyzOgen8Oq1hogJwaU
+    msg = (
+        f"BW subscription by: {customer_email} for org.id: {client_reference_id}\n"
+        f"    status: {payment_status}, {amount_total} {currency}\n"
+        f"    subscription: {subscription_id}, invoice: {invoice_id}\n"
     )
-    # need: open related subscription or product ?.
+    print(msg, file=sys.stderr)
+
+
+def _register_subscription(data_obj: dict[str, Any]) -> None:
+    subscription_id = data_obj["subscription"]
+    sub_content = stripe.Subscription.retrieve(subscription_id)
+    print(sub_content, file=sys.stderr)
+
+    # sample subscription from doc:
+    # {
+    #   "id": "sub_1MowQVLkdIwHu7ixeRlqHVzs",
+    #   "object": "subscription",
+    #   "application": null,
+    #   "application_fee_percent": null,
+    #   "automatic_tax": {
+    #     "enabled": false,
+    #     "liability": null
+    #   },
+    #   "billing_cycle_anchor": 1679609767,
+    #   "billing_thresholds": null,
+    #   "cancel_at": null,
+    #   "cancel_at_period_end": false,
+    #   "canceled_at": null,
+    #   "cancellation_details": {
+    #     "comment": null,
+    #     "feedback": null,
+    #     "reason": null
+    #   },
+    #   "collection_method": "charge_automatically",
+    #   "created": 1679609767,
+    #   "currency": "usd",
+    #   "current_period_end": 1682288167,
+    #   "current_period_start": 1679609767,
+    #   "customer": "cus_Na6dX7aXxi11N4",
+    #   "days_until_due": null,
+    #   "default_payment_method": null,
+    #   "default_source": null,
+    #   "default_tax_rates": [],
+    #   "description": null,
+    #   "discount": null,
+    #   "discounts": null,
+    #   "ended_at": null,
+    #   "invoice_settings": {
+    #     "issuer": {
+    #       "type": "self"
+    #     }
+    #   },
+    #   "items": {
+    #     "object": "list",
+    #     "data": [
+    #       {
+    #         "id": "si_Na6dzxczY5fwHx",
+    #         "object": "subscription_item",
+    #         "billing_thresholds": null,
+    #         "created": 1679609768,
+    #         "metadata": {},
+    #         "plan": {
+    #           "id": "price_1MowQULkdIwHu7ixraBm864M",
+    #           "object": "plan",
+    #           "active": true,
+    #           "aggregate_usage": null,
+    #           "amount": 1000,
+    #           "amount_decimal": "1000",
+    #           "billing_scheme": "per_unit",
+    #           "created": 1679609766,
+    #           "currency": "usd",
+    #           "discounts": null,
+    #           "interval": "month",
+    #           "interval_count": 1,
+    #           "livemode": false,
+    #           "metadata": {},
+    #           "nickname": null,
+    #           "product": "prod_Na6dGcTsmU0I4R",
+    #           "tiers_mode": null,
+    #           "transform_usage": null,
+    #           "trial_period_days": null,
+    #           "usage_type": "licensed"
+    #         },
+    #         "price": {
+    #           "id": "price_1MowQULkdIwHu7ixraBm864M",
+    #           "object": "price",
+    #           "active": true,
+    #           "billing_scheme": "per_unit",
+    #           "created": 1679609766,
+    #           "currency": "usd",
+    #           "custom_unit_amount": null,
+    #           "livemode": false,
+    #           "lookup_key": null,
+    #           "metadata": {},
+    #           "nickname": null,
+    #           "product": "prod_Na6dGcTsmU0I4R",
+    #           "recurring": {
+    #             "aggregate_usage": null,
+    #             "interval": "month",
+    #             "interval_count": 1,
+    #             "trial_period_days": null,
+    #             "usage_type": "licensed"
+    #           },
+    #           "tax_behavior": "unspecified",
+    #           "tiers_mode": null,
+    #           "transform_quantity": null,
+    #           "type": "recurring",
+    #           "unit_amount": 1000,
+    #           "unit_amount_decimal": "1000"
+    #         },
+    #         "quantity": 1,
+    #         "subscription": "sub_1MowQVLkdIwHu7ixeRlqHVzs",
+    #         "tax_rates": []
+    #       }
+    #     ],
+    #     "has_more": false,
+    #     "total_count": 1,
+    #     "url": "/v1/subscription_items?subscription=sub_1MowQVLkdIwHu7ixeRlqHVzs"
+    #   },
+    #   "latest_invoice": "in_1MowQWLkdIwHu7ixuzkSPfKd",
+    #   "livemode": false,
+    #   "metadata": {},
+    #   "next_pending_invoice_item_invoice": null,
+    #   "on_behalf_of": null,
+    #   "pause_collection": null,
+    #   "payment_settings": {
+    #     "payment_method_options": null,
+    #     "payment_method_types": null,
+    #     "save_default_payment_method": "off"
+    #   },
+    #   "pending_invoice_item_interval": null,
+    #   "pending_setup_intent": null,
+    #   "pending_update": null,
+    #   "schedule": null,
+    #   "start_date": 1679609767,
+    #   "status": "active",
+    #   "test_clock": null,
+    #   "transfer_data": null,
+    #   "trial_end": null,
+    #   "trial_settings": {
+    #     "end_behavior": {
+    #       "missing_payment_method": "create_invoice"
+    #     }
+    #   },
+    #   "trial_start": null
+    # }
 
     # login_user(user) ...
     # db.session.commit()
