5770a6ce6a95904e0550e426f44c07d0a16c493e
Stefane Fermigier <sf@abilian.com>
2025-01-28 14:00:42 +0100

    chore: poetry->uv

--- noxfile.py
+++ noxfile.py
@@ -5,29 +5,28 @@ from __future__ import annotations
 
 import nox
 
-nox.options.sessions = ["lint", "test"]
+nox.options.default_venv_backend = "uv|virtualenv"
+nox.options.sessions = ["lint", "test", "check_prod"]
 nox.options.reuse_existing_virtualenvs = True
 
 # NB: first one is the default
-PYTHON_VERSIONS = ["3.12", "3.11"]
+PYTHON_VERSIONS = ["3.12", "3.11", "3.13"]
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def test(session: nox.Session) -> None:
-    session.install("poetry")
-    session.run("poetry", "install")
+    session.run("uv", "sync")
     session.run("pytest")
 
 
 @nox.session(python=PYTHON_VERSIONS[0])
 def lint(session: nox.Session) -> None:
-    session.install("poetry")
-    session.run("poetry", "install")
-    session.run("poetry", "run", "make", "lint")
+    session.run("uv", "sync")
+    session.run("uv", "run", "make", "lint")
 
 
 @nox.session(python=PYTHON_VERSIONS[0])
 def check_prod(session: nox.Session) -> None:
-    session.install("poetry")
-    session.run("poetry", "install", "--only", "main")
-    session.run("flask inspect")
+    session.install(".")
+    session.run("uv", "run", "flask", "inspect")
+    # session.run("flask inspect")
