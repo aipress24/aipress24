943d89f49c31700049c7d3bd6d6b453a3853a718
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-01-31 18:00:07 +0100

    feat: add Stripe events customer.subscription.* to webhook (wip)

--- src/app/modules/stripe/views/webhook.py
+++ src/app/modules/stripe/views/webhook.py
@@ -32,11 +32,11 @@ class SubscriptionInfo:
     """Utility class to store synthetic information about some presumably
     Stripe BW subscription."""
 
+    subscription_id: str = ""
     customer_email: str = ""
     payment_status: str = ""
     client_reference_id: str = ""
     invoice_id: str = ""
-    subscription_id: str = ""
     currency: str = ""
     amount_total: Decimal = Decimal(0)
     org_type: str = ""
@@ -50,6 +50,30 @@ class SubscriptionInfo:
     product_id: str = ""
 
 
+@dataclass
+class SubsSchedule:
+    """Utility class to store synthetic information about some presumably
+    Stripe subscription_schedule."""
+
+    id: str = ""
+    # customer_email: str = ""
+    # payment_status: str = ""
+    # client_reference_id: str = ""
+    # invoice_id: str = ""
+    # subscription_id: str = ""
+    # currency: str = ""
+    # amount_total: Decimal = Decimal(0)
+    # org_type: str = ""
+    # created: int = 0
+    # current_period_start: int = 0
+    # current_period_end: int = 0
+    # price_id: str = ""
+    # name: str = ""
+    # nickname: str = ""
+    # interval: str = ""
+    # product_id: str = ""
+
+
 def info(*args: Any) -> None:
     msg = " ".join(str(x) for x in args)
     print(f"Info (Webhook): {msg}", file=sys.stderr)
@@ -58,6 +82,14 @@ def info(*args: Any) -> None:
 def warning(*args: Any) -> None:
     msg = " ".join(str(x) for x in args)
     print(f"Warning (Webhook): {msg}", file=sys.stderr)
+
+
+def retrieve_customer(customer_id: str) -> object:
+    try:
+        return stripe.Customer.retrieve(customer_id)
+    except stripe.error.StripeError as e:
+        warning(f"Error retrieving customer: {e}")
+        return {}
 
 
 @blueprint.route("/webhook", methods=["GET", "POST"])
@@ -87,19 +119,470 @@ def on_received_event(event) -> None:
     match event.type:
         case "checkout.session.completed":
             return on_checkout_session_completed(event)
+        case "subscription_schedule.aborted":
+            return on_subscription_schedule_aborted(event)
+        case "subscription_schedule.canceled":
+            return on_subscription_schedule_canceled(event)
+        case "subscription_schedule.completed":
+            return on_subscription_schedule_completed(event)
+        case "subscription_schedule.created":
+            return on_subscription_schedule_created(event)
+        case "subscription_schedule.expiring":
+            return on_subscription_schedule_expiring(event)
+        case "subscription_schedule.released":
+            return on_subscription_schedule_released(event)
+        case "subscription_schedule.updated":
+            return on_subscription_schedule_updated(event)
+        case "customer.subscription.created":
+            return on_customer_subscription_created(event)
+        case "customer.subscription.deleted":
+            return on_customer_subscription_deleted(event)
+        case "customer.subscription.paused":
+            return on_customer_subscription_paused(event)
+        case "customer.subscription.pending_update_applied":
+            return on_customer_subscription_pending_update_applied(event)
+        case "customer.subscription.pending_update_expired":
+            return on_customer_subscription_pending_update_expired(event)
+        case "customer.subscription.resumed":
+            return on_customer_subscription_resumed(event)
+        case "customer.subscription.trial_will_end":
+            return on_customer_subscription_trial_will_end(event)
+        case "customer.subscription.updated":
+            return on_customer_subscription_updated(event)
         case _:
             return unmanaged_event(event)
+
+
+def _get_event_object(event) -> object:
+    session.clear()
+    info(f"on event:{event.id}, type={event.type}")
+    data = event.data
+    return data.object
 
 
 def unmanaged_event(event) -> None:
     warning(f"event not managed: event: id={event.id}, type={event.type}")
 
 
+def on_subscription_schedule_aborted(event) -> None:
+    data_obj = _get_event_object(event)
+    subs_schedule = _parse_schedule_object(data_obj)  # noqa:F841
+
+
+def on_subscription_schedule_canceled(event) -> None:
+    data_obj = _get_event_object(event)
+    subs_schedule = _parse_schedule_object(data_obj)  # noqa:F841
+
+
+def on_subscription_schedule_completed(event) -> None:
+    data_obj = _get_event_object(event)
+    subs_schedule = _parse_schedule_object(data_obj)  # noqa:F841
+
+
+def on_subscription_schedule_created(event) -> None:
+    # {
+    # "object": {
+    # "id":
+    # "sub_1QnLgdIyzOgen8Oqn4o3I09v"
+    # ,
+    # "object":
+    # "subscription",
+    # "application":
+    # null,
+    # "application_fee_percent":
+    # null,
+    # "automatic_tax": {
+    # "disabled_reason":
+    # null,
+    # "enabled":
+    # true,
+    # "liability": {
+    # "type":
+    # "self",
+    # },
+    # },
+    # "billing_cycle_anchor":
+    # 1769871555,
+    # "billing_cycle_anchor_config":
+    # null,
+    # "billing_thresholds":
+    # null,
+    # "cancel_at":
+    # null,
+    # "cancel_at_period_end":
+    # false,
+    # "canceled_at":
+    # null,
+    # "cancellation_details": {
+    # "comment":
+    # null,
+    # "feedback":
+    # null,
+    # "reason":
+    # null,
+    # },
+    # "collection_method":
+    # "charge_automatically",
+    # "created":
+    # 1738335555
+    # ,
+    # "currency":
+    # "eur",
+    # "current_period_end":
+    # 1769871555
+    # ,
+    # "current_period_start":
+    # 1738335555
+    # ,
+    # "customer":
+    # "cus_RgdKZKVp2vhHOK"
+    # ,
+    # "days_until_due":
+    # null,
+    # "default_payment_method":
+    # "pm_1QnG3RIyzOgen8OqM4v1yE9e"
+    # ,
+    # "default_source":
+    # null,
+    # "default_tax_rates": [],
+    # "description":
+    # null,
+    # "discount":
+    # null,
+    # "discounts": [],
+    # "ended_at":
+    # null,
+    # "invoice_settings": {
+    # "account_tax_ids":
+    # null,
+    # "issuer": {
+    # "type":
+    # "self",
+    # },
+    # },
+    # "items": {
+    # "object":
+    # "list",
+    # "data": [
+    # "0": {
+    # "id":
+    # "si_Rgj8lzOMorpL7g"
+    # ,
+    # "object":
+    # "subscription_item",
+    # "billing_thresholds":
+    # null,
+    # "created":
+    # 1738335556
+    # ,
+    # "discounts": [],
+    # "metadata": {},
+    # "plan": {
+    # "id":
+    # "price_1QU4SdIyzOgen8OqacVy58Us"
+    # ,
+    # "object":
+    # "plan",
+    # "active":
+    # true,
+    # "aggregate_usage":
+    # null,
+    # "amount":
+    # null,
+    # "amount_decimal":
+    # null,
+    # "billing_scheme":
+    # "tiered",
+    # "created":
+    # 1733741107
+    # ,
+    # "currency":
+    # "eur",
+    # "interval":
+    # "year",
+    # "interval_count":
+    # 1,
+    # "livemode":
+    # false,
+    # "metadata": {},
+    # "meter":
+    # null,
+    # "nickname":
+    # "BW4Orga Tarif annuel",
+    # "product":
+    # "prod_RL39N6QDHt4Cvf"
+    # ,
+    # "tiers_mode":
+    # "graduated",
+    # "transform_usage":
+    # null,
+    # "trial_period_days":
+    # null,
+    # "usage_type":
+    # "licensed",
+    # },
+    # "price": {
+    # "id":
+    # "price_1QU4SdIyzOgen8OqacVy58Us"
+    # ,
+    # "object":
+    # "price",
+    # "active":
+    # true,
+    # "billing_scheme":
+    # "tiered",
+    # "created":
+    # 1733741107
+    # ,
+    # "currency":
+    # "eur",
+    # "custom_unit_amount":
+    # null,
+    # "livemode":
+    # false,
+    # "lookup_key":
+    # "bw4orga_y",
+    # "metadata": {},
+    # "nickname":
+    # "BW4Orga Tarif annuel",
+    # "product":
+    # "prod_RL39N6QDHt4Cvf"
+    # ,
+    # "recurring": {
+    # "aggregate_usage":
+    # null,
+    # "interval":
+    # "year",
+    # "interval_count":
+    # 1,
+    # "meter":
+    # null,
+    # "trial_period_days":
+    # null,
+    # "usage_type":
+    # "licensed",
+    # },
+    # "tax_behavior":
+    # "exclusive",
+    # "tiers_mode":
+    # "graduated",
+    # "transform_quantity":
+    # null,
+    # "type":
+    # "recurring",
+    # "unit_amount":
+    # null,
+    # "unit_amount_decimal":
+    # null,
+    # },
+    # "quantity":
+    # 1,
+    # "subscription":
+    # "sub_1QnLgdIyzOgen8Oqn4o3I09v"
+    # ,
+    # "tax_rates": [],
+    # },
+    # ],
+    # "has_more":
+    # false,
+    # "total_count":
+    # 1,
+    # "url":
+    # "/v1/subscription_items?subscription=sub_1QnLgdIyzOgen8Oqn4o3I09v",
+    # },
+    # "latest_invoice":
+    # "in_1QnLgdIyzOgen8OqfrnvcSvF"
+    # ,
+    # "livemode":
+    # false,
+    # "metadata": {},
+    # "next_pending_invoice_item_invoice":
+    # null,
+    # "on_behalf_of":
+    # null,
+    # "pause_collection":
+    # null,
+    # "payment_settings": {
+    # "payment_method_options":
+    # null,
+    # "payment_method_types":
+    # null,
+    # "save_default_payment_method":
+    # "off",
+    # },
+    # "pending_invoice_item_interval":
+    # null,
+    # "pending_setup_intent":
+    # null,
+    # "pending_update":
+    # null,
+    # "plan": {
+    # "id":
+    # "price_1QU4SdIyzOgen8OqacVy58Us"
+    # ,
+    # "object":
+    # "plan",
+    # "active":
+    # true,
+    # "aggregate_usage":
+    # null,
+    # "amount":
+    # null,
+    # "amount_decimal":
+    # null,
+    # "billing_scheme":
+    # "tiered",
+    # "created":
+    # 1733741107
+    # ,
+    # "currency":
+    # "eur",
+    # "interval":
+    # "year",
+    # "interval_count":
+    # 1,
+    # "livemode":
+    # false,
+    # "metadata": {},
+    # "meter":
+    # null,
+    # "nickname":
+    # "BW4Orga Tarif annuel",
+    # "product":
+    # "prod_RL39N6QDHt4Cvf"
+    # ,
+    # "tiers_mode":
+    # "graduated",
+    # "transform_usage":
+    # null,
+    # "trial_period_days":
+    # null,
+    # "usage_type":
+    # "licensed",
+    # },
+    # "quantity":
+    # 1,
+    # "schedule":
+    # null,
+    # "start_date":
+    # 1738335555
+    # ,
+    # "status":
+    # "trialing",
+    # "test_clock":
+    # null,
+    # "transfer_data":
+    # null,
+    # "trial_end":
+    # 1769871555
+    # ,
+    # "trial_settings": {
+    # "end_behavior": {
+    # "missing_payment_method":
+    # "create_invoice",
+    # },
+    # },
+    # "trial_start":
+    # 1738335555
+    # ,
+    # },
+    # "previous_attributes":
+    # null,
+    # }
+    data_obj = _get_event_object(event)
+    subs_schedule = _parse_schedule_object(data_obj)  # noqa:F841
+
+
+def on_subscription_schedule_expiring(event) -> None:
+    data_obj = _get_event_object(event)
+    subs_schedule = _parse_schedule_object(data_obj)  # noqa:F841
+
+
+def on_subscription_schedule_released(event) -> None:
+    data_obj = _get_event_object(event)
+    subs_schedule = _parse_schedule_object(data_obj)  # noqa:F841
+
+
+def on_subscription_schedule_updated(event) -> None:
+    data_obj = _get_event_object(event)
+    subs_schedule = _parse_schedule_object(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_created(event) -> None:
+    """Occurs whenever a customer is signed up for a new plan.
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_deleted(event) -> None:
+    """Occurs whenever a customer’s subscription ends.
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_paused(event) -> None:
+    """Occurs whenever a customer’s subscription is paused.
+
+    Only applies when subscriptions enter status=paused, not when
+    payment collection is paused.
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_pending_update_applied(event) -> None:
+    """Occurs whenever a customer’s subscription’s pending
+    update is applied, and the subscription is updated.
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_pending_update_expired(event) -> None:
+    """Occurs whenever a customer’s subscription’s pending update
+    expires before the related invoice is paid.
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_resumed(event) -> None:
+    """Occurs whenever a customer’s subscription is no longer paused.
+    Only applies when a status=paused subscription is resumed,
+    not when payment collection is resumed.
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_trial_will_end(event) -> None:
+    """Occurs three days before a subscription’s trial period is scheduled
+    to end, or when a trial is ended immediately (using trial_end=now).
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
+def on_customer_subscription_updated(event) -> None:
+    """Occurs whenever a subscription changes (e.g., switching from one
+    plan to another, or changing the status from trial to active).
+
+    data.object is a subscription"""
+    data_obj = _get_event_object(event)
+    subinfo = _make_customer_subscription_info(data_obj)  # noqa:F841
+
+
 def on_checkout_session_completed(event) -> None:
-    session.clear()
-    info(f"on event:{event.id}, type={event.type}")
-    data = event.data
-    data_obj = data.object
+    data_obj = _get_event_object(event)
     if not _filter_unknown_checkout(data_obj):
         return
     subinfo = _make_subscription_info(data_obj)
@@ -143,7 +626,60 @@ def _make_subscription_info(data_obj: dict[str, Any]) -> SubscriptionInfo:
     subinfo.subscription_id = data_obj["subscription"]
     subinfo.currency = data_obj["currency"]
     subinfo.amount_total = Decimal(data_obj["amount_total"]) / 100
+
+
+def _make_customer_subscription_info(data_obj: dict[str, Any]) -> SubscriptionInfo:
+    subinfo = SubscriptionInfo()
+    # print("//////// data_obj", file=sys.stderr)
+    # print(data_obj, file=sys.stderr)
+
+    # security
+    if data_obj.get("object") != "subscription":
+        msg = f"Not a Subscription {data_obj}"
+        raise ValueError(msg)
+    subinfo.subscription_id = data_obj["id"]
+    customer_id = data_obj["customer"]
+    customer = retrieve_customer(customer_id)
+    subinfo.customer_email = customer["email"]
+
+    subinfo.payment_status = ""
+    subinfo.client_reference_id = ""
+    subinfo.currency = ""
+    subinfo.invoice_id = ""
+    subinfo.amount_total = Decimal(0)
+    subinfo.org_type = ""
+    subinfo.created = 0
+    subinfo.current_period_start = 0
+    subinfo.current_period_end = 0
+    subinfo.price_id = ""
+    subinfo.name = ""
+    subinfo.nickname = ""
+    subinfo.interval = ""
+    subinfo.product_id = ""
+
     return subinfo
+
+
+def _parse_schedule_object(data_obj: dict[str, Any]) -> SubsSchedule:
+    subs = SubsSchedule()
+    print("//////// subs schedule data_obj", file=sys.stderr)
+    # print(data_obj, file=sys.stderr)
+    # subs.id = data_obj["id"]
+
+    # # get customer email
+    # customer = data_obj["customer"]
+
+    # subs.status = data_obj["status"]
+    # # not_started, active, completed, released, canceled
+    # # trialing !!
+
+    # subinfo.customer_email = data_obj["customer_email"]
+    # subinfo.payment_status = data_obj["payment_status"]
+    # subinfo.client_reference_id = data_obj["client_reference_id"]
+    # subinfo.invoice_id = data_obj["invoice"]
+    # subinfo.currency = data_obj["currency"]
+    # subinfo.amount_total = Decimal(data_obj["amount_total"]) / 100
+    return subs
 
 
 def _log_checkout_subinfo(subinfo: SubscriptionInfo) -> None:
@@ -282,3 +818,97 @@ def _get_bw_product(subinfo: SubscriptionInfo) -> stripe.Product | None:
     # same product_id as plan
     bw_products = stripe_bw_subscription_dict()
     return bw_products.get(subinfo.product_id)
+
+
+# #################################################################@
+# subscription events
+
+# subscription_schedule.aborted
+# data.object is a subscription schedule
+# Occurs whenever a subscription schedule is canceled due to the underlying subscription being canceled because of delinquency.
+
+
+# subscription_schedule.canceled
+# data.object is a subscription schedule
+# Occurs whenever a subscription schedule is canceled.
+
+
+# subscription_schedule.completed
+# data.object is a subscription schedule
+# Occurs whenever a new subscription schedule is completed.
+
+
+# subscription_schedule.created
+# data.object is a subscription schedule
+# Occurs whenever a new subscription schedule is created.
+
+
+# subscription_schedule.expiring
+# data.object is a subscription schedule
+# Occurs 7 days before a subscription schedule will expire.
+
+
+# subscription_schedule.released
+# data.object is a subscription schedule
+# Occurs whenever a new subscription schedule is released.
+
+
+# subscription_schedule.updated
+# data.object is a subscription schedule
+# Occurs whenever a subscription schedule is updated.
+
+
+# checkout.session.async_payment_failed
+# checkout.session.async_payment_succeeded
+# checkout.session.completed
+# checkout.session.expired
+# customer.subscription.created
+# customer.subscription.deleted
+# customer.subscription.paused
+# customer.subscription.pending_update_applied
+# customer.subscription.pending_update_expired
+# customer.subscription.resumed
+# customer.subscription.trial_will_end
+# customer.subscription.updated
+#
+#
+
+
+# customer.subscription.created
+# data.object is a subscription
+# Occurs whenever a customer is signed up for a new plan.
+
+
+# customer.subscription.deleted
+# data.object is a subscription
+# Occurs whenever a customer’s subscription ends.
+
+
+# customer.subscription.paused
+# data.object is a subscription
+# Occurs whenever a customer’s subscription is paused. Only applies when subscriptions enter status=paused, not when payment collection is paused.
+
+
+# customer.subscription.pending_update_applied
+# data.object is a subscription
+# Occurs whenever a customer’s subscription’s pending update is applied, and the subscription is updated.
+
+
+# customer.subscription.pending_update_expired
+# data.object is a subscription
+# Occurs whenever a customer’s subscription’s pending update expires before the related invoice is paid.
+
+
+# customer.subscription.resumed
+# data.object is a subscription
+# Occurs whenever a customer’s subscription is no longer paused. Only applies when a status=paused subscription is resumed, not when payment collection is resumed.
+
+
+# customer.subscription.trial_will_end
+# data.object is a subscription
+# Occurs three days before a subscription’s trial period is scheduled to end, or when a trial is ended immediately (using trial_end=now).
+
+
+# customer.subscription.updated
+# data.object is a subscription
+# Occurs whenever a subscription changes (e.g., switching from one plan to another, or changing the status from trial to active).
