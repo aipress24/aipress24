f7a761f5afe0ae79ca66e301dcaa38e1f5d4b75a
Stefane Fermigier <sf@abilian.com>
2025-03-27 15:42:43 +0100

    test: postgres testing script

---
+++ scripts/pg-test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import subprocess
+
+env = dict(**os.environ)
+env["TEST_DATABASE_URI"] = "postgresql://localhost/aipress24_test"
+
+
+def sh(cmd):
+    print(f"Running: {cmd}")
+    subprocess.check_call(cmd, env=env)
+
+
+sh(["dropdb", "aipress24_test"])
+sh(["createdb", "aipress24_test"])
+sh(["pytest"])
