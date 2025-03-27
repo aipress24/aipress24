b5ce9d6ced9c7f93e73cb67a8cde8146efc8a23c
Stefane Fermigier <sf@abilian.com>
2025-03-27 16:00:12 +0100

    test: pg test script

--- scripts/pg-test.py
+++ scripts/pg-test.py
@@ -6,13 +6,21 @@ env = dict(**os.environ)
 env["TEST_DATABASE_URI"] = "postgresql://localhost/aipress24_test"
 
 
-def sh(cmd):
+def sh(cmd) -> int:
     print(f"Running: {cmd}")
-    subprocess.check_call(cmd, env=env)
+    result = subprocess.run(cmd, capture_output=True, text=True, env=env)
+    return result.returncode
 
+while True:
+    sh(["dropdb", "aipress24_test"])
+    sh(["createdb", "aipress24_test"])
 
-sh(["dropdb", "aipress24_test"])
-sh(["createdb", "aipress24_test"])
+    sh(["uv", "sync"])
+    status_code = sh(["pytest"])
+    print("### Status code:", status_code)
+    status_code = sh(["pytest"])
+    print("### Status code:", status_code)
 
-sh(["pytest"])
-sh(["pytest"])
+    sh(["git", "checkout", "HEAD~1"])
+    commit_id = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
+    print("### Commit ID:", commit_id)
