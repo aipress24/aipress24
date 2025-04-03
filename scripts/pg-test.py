8895b2c3992fd66109d0cb3799d2df899e9ae558
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-04-03 11:43:33 +0200

    test: postgres test script refactor

--- scripts/pg-test.py
+++ scripts/pg-test.py
@@ -10,21 +10,30 @@ env["TEST_DATABASE_URI"] = "postgresql://localhost/aipress24_test"
 def sh(cmd) -> int:
     # print(f"Running: {cmd}")
     result = subprocess.run(cmd, capture_output=True, text=True, env=env)
-    return result.returncode
+    return result.stdout, result.returncode
 
-while True:
+
+def test_commit() -> int:
+    commit_id = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
+    print("### Commit ID:", commit_id)
     sh(["dropdb", "aipress24_test"])
     sh(["createdb", "aipress24_test"])
     sh(["uv", "sync", "--frozen"])
 
     # ignore first test run, just in case
-    sh(["pytest"])
+    sh(["pytest", "-x"])
 
-    status_code = sh(["pytest"])
+    stdout, status_code = sh(["pytest", "-x"])
     print("### Status code:", status_code)
-    if status_code == 0:
-        sys.exit()
+    if status_code > 0:
+        info = [line for line in stdout.splitlines() if line.startswith("FAILED")]
+        if info:
+            print("###", info[0])
+    return status_code
 
+
+while True:
+    if test_commit() == 0:
+        sys.exit()
+    print()
     sh(["git", "checkout", "HEAD~1"])
-    commit_id = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
-    print("### Commit ID:", commit_id)
