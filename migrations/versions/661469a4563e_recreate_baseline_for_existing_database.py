aad508f907d2b217b2a24d7e7c39efb5c26e1c91
Stefane Fermigier <sf@abilian.com>
2025-06-12 10:38:01 +0200

    chore: migrations

---
+++ migrations/versions/661469a4563e_recreate_baseline_for_existing_database.py
@@ -0,0 +1,26 @@
+"""Recreate baseline for existing database
+
+Revision ID: 661469a4563e
+Revises:
+Create Date: 2025-06-12 10:20:33.608933
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+# revision identifiers, used by Alembic.
+revision = "661469a4563e"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
