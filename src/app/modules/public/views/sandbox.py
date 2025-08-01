13ec11c0cf21448ea65ad3de640d4c109d0f4864
Stefane Fermigier <sf@abilian.com>
2025-08-01 08:08:32 +0200

    feat: maquette de page de réglage de la vente des droits.

---
+++ src/app/modules/public/views/sandbox.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2021-2024, Abilian SAS & TCA
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from devtools import debug
+from flask import current_app
+from werkzeug.exceptions import NotFound
+
+from app.modules.public import get
+
+
+@get("/sandbox/<path:path>")
+def sandbox(path: str):
+    path = path.removesuffix("/")
+
+    root = Path(current_app.root_path).parent.parent.parent / "sandbox-pages"
+    debug(root)
+    html_file = root / (path + ".html")
+    if not html_file.exists():
+        raise NotFound("File not found")
+
+    return html_file.read_text()
