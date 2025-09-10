f29d533146c9471f57a01ab059c5f23f5b25f59c
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-09-10 11:54:11 +0200

    refact: update KYCProfile attributes

--- src/app/modules/kyc/populate_profile.py
+++ src/app/modules/kyc/populate_profile.py
@@ -17,85 +17,97 @@ def default_show_contact_details() -> dict[str, str | bool]:
     return data
 
 
-def default_info_perso() -> dict[str, str | bool]:
+def default_info_perso() -> dict[str, str | list]:
     return {
         "pseudo": "",
         "no_carte_presse": "",
-        "macaron_hebergement": False,
-        "macaron_repas": False,
-        "macaron_verre": False,
+        "metier_principal": "",
+        "metier": [],
+        "metier_detail": [],
+        "competences": [],
+        "competences_journalisme": [],
+        "langues": [],
+        "formations": "",
+        "experiences": "",
     }
 
 
 def default_info_pro() -> dict[str, str | list]:
     return {
-        "adresse_pro": "",
-        "compl_adresse_pro": "",
-        "fonctions_ass_syn": [],
-        "fonctions_ass_syn_detail": [],
-        "fonctions_journalisme": [],
-        "fonctions_org_priv": [],
-        "fonctions_org_priv_detail": [],
-        "fonctions_pol_adm": [],
-        "fonctions_pol_adm_detail": [],
-        "ligne_directe": "",
-        "nom_adm": "",  #
-        "nom_agence_rp": "",
-        "nom_group_com": "",  #
-        "nom_groupe_presse": "",  #
+        "nom_groupe_presse": "",
         "nom_media": [],
         "nom_media_instit": "",
-        "nom_orga": "",
-        "pays_zip_ville": "",
-        "pays_zip_ville_detail": "",
-        "taille_orga": "",
-        "tel_standard": "",
+        "type_entreprise_media": [],
+        "type_presse_et_media": [],
+        "nom_group_com": "",
+        "nom_agence_rp": "",
         "type_agence_rp": [],
-        "type_entreprise_media": [],  #
+        "nom_adm": "",
+        "nom_orga": "",
         "type_orga": [],
         "type_orga_detail": [],
-        "type_presse_et_media": [],
+        "taille_orga": "",
+        "secteurs_activite_medias": [],
+        "secteurs_activite_medias_detail": [],
+        "secteurs_activite_rp": [],
+        "secteurs_activite_rp_detail": [],
+        "secteurs_activite_detailles": [],
+        "secteurs_activite_detailles_detail": [],
+        "pays_zip_ville": "",
+        "pays_zip_ville_detail": "",
+        "adresse_pro": "",
+        "compl_adresse_pro": "",
+        "tel_standard": "",
+        "ligne_directe": "",
         "url_site_web": "",
     }
 
 
 def default_match_making() -> dict[str, str | list]:
     return {
-        "competences": [],
-        "competences_journalisme": [],
-        "experiences": "",
-        "formations": "",
-        "hobbies": "",
-        "interet_ass_syn": [],
-        "interet_ass_syn_detail": [],
-        "interet_org_priv": [],
-        "interet_org_priv_detail": [],
+        "fonctions_journalisme": [],
+        "fonctions_pol_adm": [],
+        "fonctions_pol_adm_detail": [],
+        "fonctions_org_priv": [],
+        "fonctions_org_priv_detail": [],
+        "fonctions_ass_syn": [],
+        "fonctions_ass_syn_detail": [],
         "interet_pol_adm": [],
         "interet_pol_adm_detail": [],
-        "langues": [],
-        "metier": [],
-        "metier_detail": [],
-        "secteurs_activite_detailles": [],
-        "secteurs_activite_detailles_detail": [],
-        "secteurs_activite_medias": [],
-        "secteurs_activite_medias_detail": [],
-        "secteurs_activite_rp": [],
-        "secteurs_activite_rp_detail": [],
+        "interet_org_priv": [],
+        "interet_org_priv_detail": [],
+        "interet_ass_syn": [],
+        "interet_ass_syn_detail": [],
         "transformation_majeure": [],
         "transformation_majeure_detail": [],
     }
 
 
+def default_info_hobby() -> dict[str, str | bool]:
+    return {
+        "hobbies": "",
+        "macaron_hebergement": False,
+        "macaron_repas": False,
+        "macaron_verre": False,
+    }
+
+
 def default_business_wall() -> dict[str, bool]:
     return {
-        "trigger_academics": False,
-        "trigger_academics_entrepreneur": False,
         "trigger_media_agence_de_presse": False,
+        "trigger_media_jr_microentrep": False,
         "trigger_media_media": False,
-        "trigger_organization": False,
+        "trigger_media_federation": False,
         "trigger_pr": False,
         "trigger_pr_independant": False,
+        "trigger_pr_organisation": False,
+        "trigger_organisation": False,
+        "trigger_expert": False,
+        "trigger_startup": False,
         "trigger_transformers": False,
+        "trigger_transformers_independant": False,
+        "trigger_academics": False,
+        "trigger_academics_entrepreneur": False,
     }
 
 
@@ -109,6 +121,8 @@ def _default_dict(category: str) -> dict[str, Any]:
             default_fct = default_info_pro
         case "match_making":
             default_fct = default_match_making
+        case "info_hobby":
+            default_fct = default_info_hobby
         case "business_wall":
             default_fct = default_business_wall
         case _:
