d84b137be6237ef9a733d3f697da8bc86426b1bd
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-09-24 15:48:11 +0200

    fix: apply ruff on migration df70a0a218ae_fix_missing_field_competences

--- migrations/versions/df70a0a218ae_fix_missing_field_competences_.py
+++ migrations/versions/df70a0a218ae_fix_missing_field_competences_.py
@@ -5,12 +5,12 @@ Revises: 66eb37fc43e6
 Create Date: 2025-09-19 16:30:00.000000
 
 """
+from __future__ import annotations
 
 import json
 
+import sqlalchemy as sa
 from alembic import op
-import sqlalchemy as sa
-import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "df70a0a218ae"
