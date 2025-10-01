42a44ec95efb544b64ab4de6ac22e1bfeb071bb7
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-10-01 09:59:53 +0200

    fix: unchanged logo in the BW form do not fail

--- src/app/modules/wip/pages/business_wall/valid_bw_image.py
+++ src/app/modules/wip/pages/business_wall/valid_bw_image.py
@@ -30,33 +30,30 @@ class ValidBWImageField(FileField):
     ) -> None:
         self.max_image_size = kwargs.pop("max_image_size", 2048)  # KB
         self.data_b64 = kwargs.pop("data_b64", b"")
-        # self.preload_filename = kwargs.pop("filename", "")
+        self.preload_filename = kwargs.pop("filename", "")
         self.preload_filesize = kwargs.pop("filesize", 0)
         self.is_required = kwargs.pop("is_required", False)
         self.readonly = kwargs.pop("readonly", False)
         super().__init__(**kwargs)
         self.multiple = False
 
-    def load_data(
-        self,
-        data: bytes,
-    ) -> None:
+    def load_data(self, data: bytes) -> None:
         self.data = data or b""
         self.data_b64 = base64.standard_b64encode(self.data)
-        # self.preload_filename = filename
+        self.preload_filename = "unused"
         self.preload_filesize = len(self.data)
 
     def preloaded_image(self) -> str:
         return self.data_b64.decode()
 
-    # def id_preload_name(self) -> str:
-    #     return self.id + "_preload_name"
+    def id_preload_name(self) -> str:
+        return self.id + "_preload_name"
 
-    # def name_preload_name(self) -> str:
-    #     return self.name + "_preload_name"
+    def name_preload_name(self) -> str:
+        return self.name + "_preload_name"
 
     def id_preload_b64(self) -> str:
         return self.id + "_preload_b64"
 
-    # def name_preload_b64(self) -> str:
-    #     return self.name + "_preload_b64"
+    def name_preload_b64(self) -> str:
+        return self.name + "_preload_b64"
