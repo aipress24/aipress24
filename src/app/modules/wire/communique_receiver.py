0f10da5c4bbeccd483b7f69550046f467ada92a7
Jerome Dumonteil <jd@abilian.com>
2025-10-31 14:12:25 +0100

    refact: split Communique receiver from article_receiver.py -> communique_receiver.py

--- src/app/modules/wire/communique_receiver.py
+++ src/app/modules/wire/communique_receiver.py
@@ -10,35 +10,35 @@ from sqlalchemy import select
 from app.constants import LOCAL_TZ
 from app.flask.extensions import db
 from app.models.lifecycle import PublicationStatus
-from app.modules.wip.models import Article
-from app.modules.wire.models import ArticlePost
+from app.modules.wip.models import Communique
+from app.modules.wire.models import PressReleasePost
 from app.signals import (
-    article_published,
-    article_unpublished,
-    article_updated,
+    communique_published,
+    communique_unpublished,
+    communique_updated,
 )
 
 
-@article_published.connect
-def on_publish(article: Article) -> None:
-    post = get_post(article)
+@communique_published.connect
+def on_publish_communique(communique: Communique) -> None:
+    post = get_post(communique)
     if not post:
-        post = ArticlePost()
-        post.newsroom_id = article.id
-        post.created_at = article.created_at
+        post = PressReleasePost()
+        post.newsroom_id = communique.id
+        post.created_at = communique.created_at
         post.published_at = now(LOCAL_TZ)
 
     post.status = PublicationStatus.PUBLIC
 
-    update_post(post, article)
+    update_post(post, communique)
 
     db.session.add(post)
     db.session.flush()
 
 
-@article_unpublished.connect
-def on_unpublish(article: Article) -> None:
-    post = get_post(article)
+@communique_unpublished.connect
+def on_unpublish_communique(communique: Communique) -> None:
+    post = get_post(communique)
     if not post:
         return
     post.status = PublicationStatus.DRAFT
@@ -47,21 +47,21 @@ def on_unpublish(article: Article) -> None:
     db.session.flush()
 
 
-@article_updated.connect
-def on_update(article: Article) -> None:
-    post = get_post(article)
+@communique_updated.connect
+def on_update_communique(communique: Communique) -> None:
+    post = get_post(communique)
     if not post:
-        # Article not published yet, nothing to do
+        # Communique not published yet, nothing to do
         return
 
-    update_post(post, article)
+    update_post(post, communique)
     post.last_updated_at = now(LOCAL_TZ)
 
     db.session.add(post)
     db.session.flush()
 
 
-def update_post(post: ArticlePost, info: Article) -> None:
+def update_post(post: PressReleasePost, info: Communique) -> None:
     post.title = info.title
     post.summary = info.chapo
     post.content = info.contenu
@@ -99,8 +99,8 @@ def update_post(post: ArticlePost, info: Article) -> None:
     post.pays_zip_ville_detail = info.pays_zip_ville_detail
 
 
-def get_post(info: Article) -> ArticlePost | None:
-    stmt = select(ArticlePost).where(ArticlePost.newsroom_id == info.id)
+def get_post(info: Communique) -> PressReleasePost | None:
+    stmt = select(PressReleasePost).where(PressReleasePost.newsroom_id == info.id)
     result = db.session.execute(stmt)
     post = result.scalar_one_or_none()
     return post
