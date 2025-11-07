a680a9b509ec25c328404ef64d3de19b13dbc624
Jerome Dumonteil <jd@abilian.com>
2025-11-07 16:05:48 +0100

    feat: admin/promotions, loading select optionsi and promotion content in the editor (wip)

--- src/app/modules/admin/pages/promotions.py
+++ src/app/modules/admin/pages/promotions.py
@@ -4,13 +4,37 @@
 
 from __future__ import annotations
 
-from devtools import debug
-from flask import request
+from flask import Flask, jsonify, request
+from flask_classful import FlaskView, route
+from flask_super.registry import register
 
 from app.flask.lib.pages import page
+from app.logging import warn
 
 from .base import BaseAdminPage
 from .home import AdminHomePage
+
+PROMO_CONTENT_DB = {
+    "wire-promo-1": "Wire / promo 1",
+    "wire-promo-2": "Wire / promo 2",
+    "events-promo-1": "Events / promo 1",
+    "events-promo-2": "Events / promo 2",
+    "biz-promo-1": "Biz / promo 1",
+    "biz-promo-2": "Biz / promo 2",
+    "swork-promo-1": "Swork / promo 1",
+    "swork-promo-2": "Swork / promo 2",
+}
+
+PROMO_OPTIONS = [
+    {"value": "wire-promo-1", "label": "Wire / promo 1"},
+    {"value": "wire-promo-2", "label": "Wire / promo 2"},
+    {"value": "events-promo-1", "label": "Events / promo 1"},
+    {"value": "events-promo-2", "label": "Events / promo 2"},
+    {"value": "biz-promo-1", "label": "Biz / promo 1"},
+    {"value": "biz-promo-2", "label": "Biz / promo 2"},
+    {"value": "swork-promo-1", "label": "Swork / promo 1"},
+    {"value": "swork-promo-2", "label": "Swork / promo 2"},
+]
 
 
 @page
@@ -25,10 +49,41 @@ class AdminPromotionsPage(BaseAdminPage):
     parent = AdminHomePage
 
     def context(self):
-        return {}
+        return {
+            "promo_options": PROMO_OPTIONS,
+        }
 
     def post(self) -> None:
         data = dict(request.form)
-        debug(data)
-        # TODO
-        # return
+        warn(data)
+
+
+class LoadContentView(FlaskView):
+    route_base = "/admin"
+
+    @route("/promotions/load", methods=["POST"])
+    def load_content(self):
+        if not request.is_json:
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Requête invalide (JSON attendu).",
+                }
+            ), 400
+
+        data = request.get_json()
+        promo_key = data.get("promo_key")
+
+        if not promo_key:
+            return jsonify(
+                {"success": False, "message": "Identifiant de promo manquant."}
+            ), 400
+
+        content = "test data"
+        # content = get(promo_key) or ""
+        return jsonify({"success": True, "content": content})
+
+
+@register
+def register_on_app(app: Flask) -> None:
+    LoadContentView.register(app)
