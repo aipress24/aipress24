940a9d97e9f2a143d6b36b6443eaf79d4d4056be
Stefane Fermigier <sf@abilian.com>
2025-11-14 16:31:46 +0100

    migration

---
+++ migrations/versions/74affc1def6c_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 74affc1def6c
+Revises:
+Create Date: 2025-11-14 16:21:43.608883
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+# revision identifiers, used by Alembic.
+revision = "74affc1def6c"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
