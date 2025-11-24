13d3400d808a491c6e107d5fb7f6d1e1ad4c78e8
Jerome Dumonteil <jd@abilian.com>
2025-11-24 17:50:20 +0100

    chore: use advanced_alchemy FileObject to store Communique images (basic implementation)

--- src/app/modules/wip/crud/cbvs/communiques.py
+++ src/app/modules/wip/crud/cbvs/communiques.py
@@ -4,13 +4,14 @@
 
 from __future__ import annotations
 
+from io import BytesIO
 from typing import cast
 
-from flask import (
+from advanced_alchemy.types.file_object import FileObject
+from flask import (  # make_response,
     Flask,
     flash,
     g,
-    make_response,
     redirect,
     render_template,
     request,
@@ -18,15 +19,18 @@ from flask import (
 )
 from flask_classful import route
 from flask_super.registry import register
-from svcs.flask import container
 from werkzeug.exceptions import NotFound
 
 from app.flask.extensions import db
-from app.flask.lib.constants import EMPTY_PNG
 from app.flask.routing import url_for
+from app.logging import warn
 from app.models.lifecycle import PublicationStatus
-from app.modules.wip.models import ComImage, Communique, CommuniqueRepository
-from app.services.blobs import BlobService
+from app.modules.wip.models import (
+    ComImage,
+    ComImageRepository,
+    Communique,
+    CommuniqueRepository,
+)
 from app.settings.constants import MAX_IMAGE_SIZE
 from app.signals import communique_published, communique_unpublished, communique_updated
 
@@ -196,13 +200,11 @@ class CommuniquesWipView(BaseWipView):
 
     def _add_image(self, communique: Communique):
         communique_repo = self._get_repo()
-        blob_service = container.get(BlobService)
+        image_repo = ComImageRepository(session=db.session)
 
         image = request.files["image"]
-        caption = request.form.get("caption", "").strip()
-        copyright = request.form.get("copyright", "").strip()
-
         image_bytes = image.read()
+
         if not image_bytes:
             flash("L'image est vide")
             return redirect(url_for("CommuniquesWipView:images", id=communique.id))
@@ -210,15 +212,29 @@ class CommuniquesWipView(BaseWipView):
             flash("L'image est trop volumineuse")
             return redirect(url_for("CommuniquesWipView:images", id=communique.id))
 
-        blob = blob_service.save(image_bytes)
+        image_filename = image.filename or "noname.jpg"
+        image_content_type = image.content_type or "application/binary"
+        warn(image_filename, image_content_type, len(image_bytes))
+        caption = request.form.get("caption", "").strip()
+        copyright = request.form.get("copyright", "").strip()
 
-        image = ComImage(
+        image_file_object = FileObject(
+            content=image_bytes,
+            filename=image_filename,
+            content_type=image_content_type,
+            backend="local",
+        )
+        image_file_object.save()
+        com_image = ComImage(
             caption=caption,
             copyright=copyright,
-            blob_id=blob.id,
+            content=image_file_object,
             owner=communique.owner,
+            communique_id=communique.id,
         )
-        communique.add_image(image)
+
+        image_repo.add(com_image)
+        communique.add_image(com_image)
         communique_repo.update(communique, auto_commit=False)
         db.session.commit()
         referrer_url = request.referrer or "/"
@@ -234,14 +250,26 @@ class CommuniquesWipView(BaseWipView):
         else:
             raise NotFound
 
-        blob_service = container.get(BlobService)
-        try:
-            blob_path = blob_service.get_path(image.blob_id)
-            return send_file(blob_path)
-        except FileNotFoundError:
-            response = make_response(EMPTY_PNG)
-            response.headers.set("Content-Type", "image/png")
-            return response
+        stored_file = image.content
+        warn(stored_file)
+        file_bytes = stored_file.get_content()
+        file_like_object = BytesIO(file_bytes)
+        # if stored_file.path:
+        #     file_bytes = Path(stored_file.path).read_bytes()
+        # else:
+        #     url = stored_file.sign()
+        #     file_bytes = requests.get(url, timeout=30).content
+
+        return send_file(
+            file_like_object,
+            mimetype=stored_file.content_type,
+            download_name=stored_file.filename,
+        )
+
+        # except FileNotFoundError:
+        #     response = make_response(EMPTY_PNG)
+        #     response.headers.set("Content-Type", "image/png")
+        #     return response
 
     @route("/<int:communique_id>/images/<int:image_id>/delete", methods=["POST"])
     def delete_image(self, communique_id: int, image_id: int):
@@ -251,6 +279,12 @@ class CommuniquesWipView(BaseWipView):
             raise NotFound
 
         communique.delete_image(image)
+        if image.content:
+            try:
+                image.content.delete_storage()
+                warn(f"Success deleted file for ComImage {image_id}")
+            except Exception as e:
+                warn(f"Could not delete file {image_id}: {e}")
         db.session.delete(image)
         db.session.commit()
 
