4c1e6423ea00a03c410dfa46b48e5b330cc4ead7
Jerome Dumonteil <jd@abilian.com>
2026-01-08 13:59:41 +0100

    feat: set the duration of notifications 2->4 seconds

--- vite/src/alpine-toasts.js
+++ vite/src/alpine-toasts.js
@@ -1,42 +1,42 @@
 function setupToasts(Alpine) {
-  Alpine.store("toasts", {
-    counter: 0,
+    Alpine.store('toasts', {
+        counter: 0,
 
-    list: [],
+        list: [],
 
-    createToast(message, type = "info") {
-      const index = this.list.length;
-      const totalVisible =
-        this.list.filter((toast) => {
-          return toast.visible;
-        }).length + 1;
-      const toast = {
-        id: this.counter++,
-        message,
-        type,
-        visible: true,
-      };
-      this.list.push(toast);
-      setTimeout(() => {
-        this.destroyToast(index);
-      }, 2000 * totalVisible);
-    },
+        createToast(message, type = 'info') {
+            const index = this.list.length;
+            const totalVisible =
+                this.list.filter((toast) => {
+                    return toast.visible;
+                }).length + 1;
+            const toast = {
+                id: this.counter++,
+                message,
+                type,
+                visible: true,
+            };
+            this.list.push(toast);
+            setTimeout(() => {
+                this.destroyToast(index);
+            }, 4000 * totalVisible);
+        },
 
-    destroyToast(index) {
-      this.list[index].visible = false;
-    },
-  });
+        destroyToast(index) {
+            this.list[index].visible = false;
+        },
+    });
 
-  document.body.addEventListener("showToast", function (e) {
-    const message = e.detail.value;
-    Alpine.store("toasts").createToast(message);
-  });
+    document.body.addEventListener('showToast', function (e) {
+        const message = e.detail.value;
+        Alpine.store('toasts').createToast(message);
+    });
 
-  if (window.toasts) {
-    for (let i = 0; i < window.toasts.length; i++) {
-      Alpine.store("toasts").createToast(window.toasts[i]);
+    if (window.toasts) {
+        for (let i = 0; i < window.toasts.length; i++) {
+            Alpine.store('toasts').createToast(window.toasts[i]);
+        }
     }
-  }
 }
 
 export default setupToasts;
