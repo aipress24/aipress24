13f04dc851fd3401b57c265fa225663910c0b848
Stefane Fermigier <sf@abilian.com>
2026-01-17 11:19:48 +0100

    chore: CI config

--- noxfile.py
+++ noxfile.py
@@ -5,28 +5,32 @@ from __future__ import annotations
 
 import nox
 
-nox.options.default_venv_backend = "uv|virtualenv"
-nox.options.sessions = ["lint", "test", "check_prod"]
-nox.options.reuse_existing_virtualenvs = True
+# nox.options.default_venv_backend = "uv"
+# nox.options.reuse_existing_virtualenvs = True
 
 # NB: first one is the default
-PYTHON_VERSIONS = ["3.12", "3.11", "3.13"]
+PYTHONS = ["3.12", "3.11", "3.13", "3.14"]
 
 
-@nox.session(python=PYTHON_VERSIONS)
-def test(session: nox.Session) -> None:
-    session.run("uv", "sync")
+@nox.session(python=PYTHONS)
+def tests(session: nox.Session) -> None:
+    uv_sync(session)
     session.run("pytest")
 
 
-@nox.session(python=PYTHON_VERSIONS[0])
+@nox.session(python=PYTHONS[0])
 def lint(session: nox.Session) -> None:
-    session.run("uv", "sync")
-    session.run("uv", "run", "make", "lint")
+    uv_sync(session)
+    session.run("uv", "run", "--active", "make", "lint")
 
 
-@nox.session(python=PYTHON_VERSIONS[0])
+@nox.session(python=PYTHONS[0])
 def check_prod(session: nox.Session) -> None:
-    session.install(".")
     session.run("uv", "run", "flask", "inspect")
-    # session.run("flask inspect")
+
+
+#
+# Utils
+#
+def uv_sync(session: nox.Session):
+    session.run("uv", "sync", "--all-groups", "--all-extras", "--active", external=True)
