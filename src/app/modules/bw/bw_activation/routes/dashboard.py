eb28b342b9c3f4ddf9c41e6bd87e8cbef676bf7e
Jerome Dumonteil <jd@abilian.com>
2026-02-17 15:30:47 +0100

    feat: add a page displaying basic information on current BW

--- src/app/modules/bw/bw_activation/routes/dashboard.py
+++ src/app/modules/bw/bw_activation/routes/dashboard.py
@@ -6,15 +6,31 @@
 
 from __future__ import annotations
 
-from flask import redirect, render_template, session, url_for
+from typing import TYPE_CHECKING, cast
 
+from flask import g, redirect, render_template, session, url_for
+
+from app.logging import warn
 from app.modules.bw.bw_activation import bp
 from app.modules.bw.bw_activation.config import BW_TYPES
+from app.modules.bw.bw_activation.user_utils import current_business_wall
+from app.modules.bw.bw_activation.utils import fill_session
+
+if TYPE_CHECKING:
+    from app.models.auth import User
 
 
 @bp.route("/dashboard")
 def dashboard():
     """Business Wall management dashboard (after activation)."""
+    user = cast("User", g.user)
+    current_bw = current_business_wall(user)
+    if current_bw:
+        warn(current_bw)
+        fill_session(current_bw)
+        if current_bw.owner_id != user.id:
+            # not enough right to manage BW (not owner)
+            return redirect(url_for("bw_activation.information"))
     if not session.get("bw_activated") or not session.get("bw_type"):
         return redirect(url_for("bw_activation.index"))
 
