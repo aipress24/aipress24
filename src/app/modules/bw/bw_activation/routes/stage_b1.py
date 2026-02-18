cdd92bbfc07f445ec299bf8916ab905e963ea53c
Jerome Dumonteil <jd@abilian.com>
2026-02-18 11:41:38 +0100

    feat: BW stage 1 membership (wip)

--- src/app/modules/bw/bw_activation/routes/stage_b1.py
+++ src/app/modules/bw/bw_activation/routes/stage_b1.py
@@ -6,7 +6,7 @@
 
 from __future__ import annotations
 
-from typing import cast
+from typing import TYPE_CHECKING, cast
 
 from flask import g, redirect, render_template, session, url_for
 
@@ -17,47 +17,41 @@ from app.modules.bw.bw_activation import bp
 from app.modules.bw.bw_activation.config import BW_TYPES
 from app.modules.bw.bw_activation.models import BWRoleType
 from app.modules.bw.bw_activation.user_utils import current_business_wall
+from app.modules.bw.bw_activation.utils import fill_session
+
+if TYPE_CHECKING:
+    from app.models.auth import User
 
 
 @bp.route("/manage-organisation-members")
 def manage_organisation_membsers():
     """Stage 1: Manage members of Business Wall organisation."""
-    if not session.get("bw_activated"):
+    # at this stage the BW must be created
+    user = cast("User", g.user)
+    current_bw = current_business_wall(user)
+    if not current_bw:
+        warn("current BW not found")
+        return ""
+
+    warn("current BW", current_bw)
+    fill_session(current_bw)
+    # fixme: here test we are owner  or manager
+
+    if not session.get("bw_activated") or not session.get("bw_type"):
         return redirect(url_for("bw_activation.index"))
 
     bw_type = session["bw_type"]
     bw_info = BW_TYPES.get(bw_type, {})
 
-    # Get current business wall and owner information
-    user = cast("User", g.user)
-    warn("user", user)
-    business_wall = current_business_wall(user)
-    if not business_wall:
-        # should be impossible, it was at least just created
+    org = current_bw.get_organisation()
+    # organisation must be created for the BW (it was created à BW creation is missing)
+    if not org:
+        warn("current BW Organisation not found")
         return ""
-    owner_info = None
-    warn("BW", business_wall)
-    if business_wall.role_assignments:
-        warn(business_wall.role_assignments)
-        for assignment in business_wall.role_assignments:
-            warn(assignment)
-            if assignment.role_type == BWRoleType.BW_OWNER.value:
-                try:
-                    owner_user = get_obj(assignment.user_id, User)
-                    owner_info = {
-                        "email": owner_user.email,
-                        "full_name": owner_user.full_name,
-                    }
-                except Exception:
-                    owner_info = {
-                        "email": "N/A",
-                        "full_name": "Inconnu",
-                    }
-                break
 
     return render_template(
         "bw_activation/B01_manage_organisation_members.html",
         bw_type=bw_type,
         bw_info=bw_info,
-        owner_info=owner_info,
+        owner_info="",
     )
