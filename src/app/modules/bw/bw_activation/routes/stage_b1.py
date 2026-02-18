726a26f715f5cd532d523e0855a1a410e33986fb
Jerome Dumonteil <jd@abilian.com>
2026-02-18 16:50:57 +0100

    refact: swap BW steps 1 and 2 (ivitations before members list)

--- src/app/modules/bw/bw_activation/routes/stage_b1.py
+++ src/app/modules/bw/bw_activation/routes/stage_b1.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
-"""Stage B1: Manage organisation members."""
+"""Stage B1: Invite organisation members."""
 
 from __future__ import annotations
 
@@ -12,8 +12,8 @@ from flask import g, redirect, render_template, request, session, url_for
 from werkzeug import Response
 
 from app.flask.extensions import db
-from app.logging import warn
-from app.modules.admin.org_email_utils import change_members_emails
+from app.modules.admin.invitations import emails_invited_to_organisation
+from app.modules.admin.org_email_utils import change_invitations_emails
 from app.modules.bw.bw_activation import bp
 from app.modules.bw.bw_activation.config import BW_TYPES
 from app.modules.bw.bw_activation.user_utils import current_business_wall
@@ -29,9 +29,9 @@ if TYPE_CHECKING:
     from app.models.auth import User
 
 
-@bp.route("/manage-organisation-members", methods=["GET", "POST"])
-def manage_organisation_members():
-    """Stage B1: Manage members of Business Wall organisation."""
+@bp.route("/invite-organisation-members", methods=["GET", "POST"])
+def invite_organisation_members():
+    """Stage B1: Manage invitations to join Business Wall organisation."""
     # at this stage the BW must be created
     user = cast("User", g.user)
     current_bw = current_business_wall(user)
@@ -57,24 +57,23 @@ def manage_organisation_members():
 
     if request.method == "POST":
         action = request.form.get("action")
-        if action == "change_emails":
+        if action == "change_invitations_emails":
             raw_mails = request.form["content"]
-            change_members_emails(org, raw_mails, remove_only=True)
+            change_invitations_emails(org, raw_mails)
+            db.session.commit()
             response = Response("")
             response.headers["HX-Redirect"] = url_for(
-                "bw_activation.manage_organisation_members"
+                "bw_activation.invite_organisation_members"
             )
-            db.session.commit()
             return response
 
-    members = list(org.members) if org else []
-    warn(members)
+    invitations_emails = emails_invited_to_organisation(org.id) if org else []
 
     return render_template(
-        "bw_activation/B01_manage_organisation_members.html",
+        "bw_activation/B01_invite_organisation_members.html",
         bw_type=bw_type,
         bw_info=bw_info,
         business_wall=current_bw,
         org=org,
-        members=members,
+        invitations_emails=invitations_emails,
     )
