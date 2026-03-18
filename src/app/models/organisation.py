e664898b9f8114cbec6574e89cc649cd8ec43469
Jerome Dumonteil <jd@abilian.com>
2026-03-18 14:37:53 +0100

    refact: remove deprecated Organisation.managers

--- src/app/models/organisation.py
+++ src/app/models/organisation.py
@@ -283,10 +283,6 @@ class Organisation(IdMixin, LifeCycleMixin, Addressable, Base):
         return self.type == OrganisationTypeEnum.AUTO or not self.active
 
     @hybrid_property
-    def managers(self) -> list[User]:
-        return [user for user in self.members if user.is_manager]
-
-    @hybrid_property
     def code_postal(self) -> str:
         """Return the zip code"""
         if not self.pays_zip_ville_detail:
