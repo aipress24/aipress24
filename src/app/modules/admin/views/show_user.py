929c8635b5ee55602d59b75d811b419e57706e2b
Jerome Dumonteil <jd@abilian.com>
2026-03-18 11:03:19 +0100

    feat: display name of organisation and BW on page admin/show_user

--- src/app/modules/admin/views/show_user.py
+++ src/app/modules/admin/views/show_user.py
@@ -6,7 +6,7 @@
 
 from __future__ import annotations
 
-from typing import ClassVar
+from typing import ClassVar, cast
 
 from arrow import now
 from flask import Response, render_template, request, url_for
@@ -19,7 +19,11 @@ from app.flask.sqla import get_obj
 from app.models.auth import User
 from app.modules.admin import blueprint
 from app.modules.admin.utils import gc_organisation, remove_user_organisation
+from app.modules.bw.bw_activation.user_utils import (
+    get_active_business_wall_for_organisation,
+)
 from app.modules.kyc.views import admin_info_context
+from app.ui.labels import LABELS_BW_TYPE_V2
 
 
 class ShowUserView(MethodView):
@@ -30,12 +34,19 @@ class ShowUserView(MethodView):
     ]
 
     def get(self, uid: str):
-        user = get_obj(uid, User)
+        user = cast(User, get_obj(uid, User))
+        org = user.organisation
+        if org:
+            active_bw = get_active_business_wall_for_organisation(org)
+        else:
+            active_bw = None
         context = admin_info_context(user)
         context.update(
             {
                 "user": user,
-                "org": user.organisation,
+                "org": org,
+                "active_bw": active_bw,
+                "LABELS_BW_TYPE_V2": LABELS_BW_TYPE_V2,
                 "title": "Informations sur l'utilisateur",
             }
         )
