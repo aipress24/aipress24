7387dc5bd832df94252d419ab1cf4b29abdcdb0f
Jerome Dumonteil <jd@abilian.com>
2026-04-01 16:32:40 +0200

    test: enable again test_guard_true_for_non_auto()

--- tests/c_e2e/modules/swork/test_swork_group_org_pages.py
+++ tests/c_e2e/modules/swork/test_swork_group_org_pages.py
@@ -521,10 +521,9 @@ class TestOrgEventsTab:
         tab = OrgEventsTab(org=auto_organisation)
         assert tab.guard() is False
 
-    @pytest.mark.skip(reason="FIXME: There is no more Organisation type / AUTO")
-    def test_guard_true_for_non_auto(self, test_organisation: Organisation):
+    def test_guard_true_for_non_auto(self, test_organisation_media: Organisation):
         """Test guard returns True for non-AUTO organisation."""
-        tab = OrgEventsTab(org=test_organisation)
+        tab = OrgEventsTab(org=test_organisation_media)
         assert tab.guard() is True
 
 
